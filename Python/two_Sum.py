32f490df1225791e668263ef1b500a7754609be5
Wen Qiao <66670158+qiao0313@users.noreply.github.com>
2021-02-22 15:47:29 +0800

    Update and rename twoSum.py to two_Sum.py

--- Python/two_Sum.py
+++ Python/two_Sum.py
@@ -4,9 +4,9 @@
 你可以按任意顺序返回答案。
 
 example:
-给定 nums = [2, 7, 11, 15], target = 9；
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
+输入： nums = [2, 7, 11, 15], target = 9；
+输出： [0, 1]；
+解释： nums[0] + nums[1] = 2 + 7 = 9
 """
 
 class Solution(object):
